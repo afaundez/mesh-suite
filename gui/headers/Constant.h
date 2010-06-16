145deafcfabddcec4824f270e0e0b4d0e7a1dc7f
Álvaro Faúndez <alvarorocks@gmail.com>
2010-06-16 07:01:37 -0400

    ADD constants

---
+++ gui/headers/Constant.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#endif // CONSTANT_H
+
+namespace Constant{
+    enum GLTriangleType{ SELECTED, NOT_SELECTED};
+    enum IncludeType{ NOT_INCLUDED, INCLUDED, BORDER_INCLUDED};
+}
